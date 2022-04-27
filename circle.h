cfa29e94fa9661bf17a558290c9b276d852358f6
telamones <vvtatatavv@gmail.com>
2022-04-27 16:20:45 +0300

    changes

--- circle.h
+++ circle.h
@@ -7,37 +7,28 @@
 using namespace std;
 
 class Circle {
-private:
     double x;
     double y;
     double r;
 
 public:
-    Circle() {
-        x=y=r=0;
-    };
-    Circle(double xx) {
-        x=xx;
-        y=r=0;
-    };
-    Circle(double xx, double yy) {
-        x=xx;
-        y=yy;
-        r=0;
-    };
-    Circle(double xx, double yy, double rr) {
+    Circle(double xx = 0, double yy = 0, double rr = 0) {
         x=xx;
         y=yy;
         r=rr;
+        cout << this->x << this->y << this->r << endl;
     };
 
     Circle (const Circle& obj) {
+        cout << this->x << this->y << this->r << endl;
         this->x = obj.x;
         this->y = obj.y;
         this->r = obj.r;
+        cout << obj.x << obj.y << obj.r << endl;
     };
 
     ~Circle() {
+        cout << this << " destr" << endl;
     };
 };
 
--- main.cpp
+++ main.cpp
@@ -1,10 +1,6 @@
 #include "circle.h"
 
 int main() {
-    double x1,y1,r1;
-    cout << "enter centre and radius of circle #1:";
-    cin >> x1 >> y1 >> r1;
-
-    Circle a(x1, y1, r1);
+    Circle a(2, 3, 6);
     Circle b(a);
 }
